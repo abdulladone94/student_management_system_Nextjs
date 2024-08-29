fa6a7e4111edca736b0ba96e5666c478a2d3e010
abdulladone94 <abdullaf@booleanlabs.biz>
2024-08-29 15:16:49 +0530

    navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -20,7 +21,8 @@ export default function DashboardLayout({
         <Menu />
       </div>
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
-        r
+        <Navbar />
+        {children}
       </div>
     </div>
   );
