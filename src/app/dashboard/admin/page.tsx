09ad9892d747fcafb23c1f3531045c28e9b6c445
abdulladone94 <abdullaf@booleanlabs.biz>
2024-08-30 10:41:36 +0530

    admin page user-cards

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,23 @@
+import UserCards from '@/components/UserCards';
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="p-4 gap-4 flex flex-col md:flex-row">
+      {/* left */}
+      <div className="w-full lg:w-2/3">
+        {/* user cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCards type="student" />
+          <UserCards type="teacher" />
+          <UserCards type="parent" />
+          <UserCards type="staff" />
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;
