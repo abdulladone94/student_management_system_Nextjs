bf732b9a09b288de9045e6a4d8dfea52599d3f69
abdulladone94 <abdullaf@booleanlabs.biz>
2024-08-30 15:12:40 +0530

    admin page count chart

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,10 +1,11 @@
+import CountChart from '@/components/CountChart';
 import UserCards from '@/components/UserCards';
 
 const AdminPage = () => {
   return (
     <div className="p-4 gap-4 flex flex-col md:flex-row">
       {/* left */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* user cards */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCards type="student" />
@@ -12,6 +13,17 @@ const AdminPage = () => {
           <UserCards type="parent" />
           <UserCards type="staff" />
         </div>
+        {/* middle charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* count chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* attendance chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* bottom charts */}
+        <div></div>
       </div>
 
       {/* right */}
