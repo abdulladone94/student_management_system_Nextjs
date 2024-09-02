1d950bdf0acc535cd696570e76798852ddaccbe3
abdulladone94 <abdullaf@booleanlabs.biz>
2024-09-02 11:35:37 +0530

    events added

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
@@ -7,11 +8,53 @@ type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+// TEMPORARY
+const events = [
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor',
+    time: '12:00 PM - 2:00 PM',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: 2,
+    title: 'Lorem ipsum dolor',
+    time: '12:00 PM - 2:00 PM',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: 3,
+    title: 'Lorem ipsum dolor',
+    time: '12:00 PM - 2:00 PM',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
+
 const EventCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
   return (
     <div className="">
       <Calendar onChange={onChange} value={value} />
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-semibold my-4">Event</h1>
+        <Image src="/moreDark.png" alt="more" width={20} height={20} />
+      </div>
+      <div>
+        {events.map((event) => (
+          <div
+            className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-zozySky even:border-t-zozyPurple"
+            key={event.id}
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600">{event.title}</h1>
+              <span className="text-gray-300 text-xs">{event.time}</span>
+            </div>
+            <div>
+              <p className="mt-2 text-sm text-gray-400">{event.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
